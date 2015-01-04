fed722e2c7abf2e8ffb27080eaeb1bcb106e5771
Eli Alejandro <iscelialejandro@gmail.com>
2015-01-04 17:33:20 -0600

    Primer Commit

---
+++ application/modules/site/controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of IndexController
+ *
+ * @author Eli Alejandro
+ */
+class Site_IndexController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+        
+    }
+}
