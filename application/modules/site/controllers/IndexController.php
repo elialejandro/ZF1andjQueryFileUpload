67ea3f9522c23776041f6c5ade8e603f3487ef36
Eli Alejandro <iscelialejandro@gmail.com>
2015-01-04 21:14:58 -0600

    Bootstrap 3 y subida de archivos

--- application/modules/site/controllers/IndexController.php
+++ application/modules/site/controllers/IndexController.php
@@ -17,4 +17,20 @@ class Site_IndexController extends Zend_Controller_Action
     {
         
     }
+    
+    public function ajaxUploadAction()
+    {
+        $this->view->layout()->disableLayout();
+        $upload_handler = new Site_Service_UploadHandler(array(
+            'upload_dir' => realpath(APPLICATION_PATH . '/../public/uploads/personal') . '/',
+            'upload_url' => $this->view->baseUrl() . '/uploads/personal/',
+            'param_name' => 'foto',
+            'image_versions' => array(
+                'thumbnail'  => array(
+                    'max_width' => 200,
+                    'max_height' => 180
+                ),
+            ),
+        ));
+    }
 }
